e7d97dcfbf18d4684a24c094d7a072b29b8488fc
majd <77127406+majdf4@users.noreply.github.com>
2024-03-07 12:15:30 -0800

    fixes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ for (let i = 1; i <= 28; i++) {
     const img = document.createElement('img')
     img.src = `./img/${i}.jpg`
     img.className = 'grid-item'
-    img.style.background = 'red'
     collage.appendChild(img)
 }
 
